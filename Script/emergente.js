b74fcb2b6de55d56ef142bec069f90df1ddaaea5
CEROMEXICO <78239365+CEROMEXICO@users.noreply.github.com>
2021-02-06 22:22:32 -0600

    Add files via upload

---
+++ Script/emergente.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: 'BIENVENIDO!!!',
+    text: 'CERO MÉXICO PROTECCIÓN EJECUTIVA',
+    imageUrl: 'IMAGESCERO/CEROOFICIAL.png',
+    background:'#858585',
+    imageWidth: 400,
+    imageHeight: 400,
+  })
+  
