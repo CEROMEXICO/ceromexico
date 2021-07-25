a6129fd9d5d20b4b94c5012c3eeaff16c4a9e8ea
CEROMEXICO <78239365+CEROMEXICO@users.noreply.github.com>
2021-07-24 21:07:51 -0500

    Delete Script directory

--- Script/emergente.js
+++
@@ -1,9 +0,0 @@
-Swal.fire({
-    title: 'BIENVENIDO!!!',
-    text: 'CERO MÉXICO PROTECCIÓN EJECUTIVA',
-    imageUrl: 'IMAGESCERO/CEROOFICIAL.png',
-    background:'#858585',
-    imageWidth: 200,
-    imageHeight: 200,
-  })
-  
